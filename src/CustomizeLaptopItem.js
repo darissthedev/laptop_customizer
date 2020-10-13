faa32aa2e80c37934617b350aa18813aece4a580
Dariss Harris <darissharris@gmail.com>
2020-10-13 13:00:26 -0700

    inital upload

---
+++ src/CustomizeLaptopItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class CustomizeLaptopItem extends Component {
+    render() {
+        return (
+            <fieldset className='feature' key={this.props.featureHash}>
+            <legend className='feature__name'>
+                <h3>{this.props.feature}
+                </h3>
+                </legend>
+                {this.props.options}    
+            </fieldset>
+        )
+    }
+}
+
+export default CustomizeLaptopItem
